e9c1f71246a99dce69b43f5ee836acd62a209ad0
Jimmy Chen <Jimmy.Chen@transport.nsw.gov.au>
2016-07-27 13:12:09 +1000

    Add messages to the screen

--- js/client.js
+++ js/client.js
@@ -4,23 +4,35 @@
   window.onload = function () {
     var inputElement = document.getElementById('input');
     var outputElement = document.getElementById('output');
+    var messageElement = document.getElementById('message');
     var imageTypeRegex = /^image\//;
 
     inputElement.addEventListener('change', fileChangeListener);
 
     function fileChangeListener() {
+      clearContents();
+
       var file = this.files[0];
       if (imageTypeRegex.test(file.type)) {
-        imageLoader.fromFile(file).then(createMosaic);
+        imageLoader.fromFile(file)
+          .then(createMosaic)
+          .then(function () {
+            messageElement.innerHTML = 'Photo mosaic successfully created.';
+          });
       } else {
-        outputElement.innerHTML = `Incorrect file type: ${file.type}`;
+        messageElement.innerHTML = `Incorrect file type: ${file.type}`;
       }
+    }
+
+    function clearContents() {
+      messageElement.innerHTML = '';
+      outputElement.innerHTML = '';
     }
 
     function createMosaic(image) {
       var mosaic = mosaicFactory.create(image, TILE_WIDTH, TILE_HEIGHT);
-      outputElement.innerHTML = '';
       outputElement.appendChild(mosaic.canvas);
+      return mosaic.processingComplete;
     }
   };
 
