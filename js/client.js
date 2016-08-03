720f85655c06346e7c7ed5f0b973d6a55e2147a5
Jimmy Chen <jchen.tc@gmail.com>
2016-08-03 13:46:37 +1000

    Convert num to string and code reformat

--- js/client.js
+++ js/client.js
@@ -1,41 +1,42 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  window.onload = function () {
-    var inputElement = document.getElementById('input');
-    var outputElement = document.getElementById('output');
-    var messageElement = document.getElementById('message');
-    var imageTypeRegex = /^image\//;
+    window.onload = function () {
+        var inputElement = document.getElementById('input');
+        var outputElement = document.getElementById('output');
+        var messageElement = document.getElementById('message');
+        var imageTypeRegex = /^image\//;
 
-    inputElement.addEventListener('change', fileChangeListener);
+        inputElement.addEventListener('change', fileChangeListener);
 
-    function fileChangeListener() {
-      clearContents();
+        function fileChangeListener() {
+            clearContents();
 
-      var file = this.files[0];
-      if (imageTypeRegex.test(file.type)) {
-        var start = performance.now();
-        imageLoader.fromFile(file)
-          .then(createMosaic)
-          .then(function () {
-            var end = performance.now();
-            messageElement.innerHTML = `Photo mosaic successfully created in ${end - start}ms.`;
-          });
-      } else {
-        messageElement.innerHTML = `Incorrect file type: ${file.type}`;
-      }
-    }
+            var file = this.files[0];
+            if (imageTypeRegex.test(file.type)) {
+                var start = performance.now();
+                imageLoader.fromFile(file)
+                    .then(createMosaic)
+                    .then(function () {
+                        var end = performance.now();
+                        var duration = (end - start).toFixed(2);
+                        messageElement.innerHTML = `Photo mosaic successfully created in ${duration} ms.`;
+                    });
+            } else {
+                messageElement.innerHTML = `Incorrect file type: ${file.type}`;
+            }
+        }
 
-    function clearContents() {
-      messageElement.innerHTML = '';
-      outputElement.innerHTML = '';
-    }
+        function clearContents() {
+            messageElement.innerHTML = '';
+            outputElement.innerHTML = '';
+        }
 
-    function createMosaic(image) {
-      var mosaic = mosaicFactory.create(image, TILE_WIDTH, TILE_HEIGHT);
-      outputElement.appendChild(mosaic.canvas);
-      return mosaic.processingComplete;
-    }
-  };
+        function createMosaic(image) {
+            var mosaic = mosaicFactory.create(image, TILE_WIDTH, TILE_HEIGHT);
+            outputElement.appendChild(mosaic.canvas);
+            return mosaic.processingComplete;
+        }
+    };
 
 })();
